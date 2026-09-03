78724dbc408a7b6902223190c8cf50991a29d041
OzOzz <caiohmbf07@gmail.com>
2026-09-03 14:57:38 -0300

    Variaveis e useState

--- introducao/src/App.jsx
+++ introducao/src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
+import { useState } from "react"
 
+function App() {
+  let [cliques, setCliques] = useState(0)
+  let [nome, setNome] = useState("Aguardando confirmação")
   return (
     <div>
       <h1>Hello world</h1>
-      <p>I'm learn react XD</p>
+      <p>Estou aprendendo React XD</p>
+
+      <hr/>
+
+      <p>Meu nome é: {nome}</p>
+      <button onClick={()=> setNome(nome = "Caio Henrique de Matos")}>Mostrar nome</button>
+
+      <hr/>
+
+      <p>Você clicou {cliques} vezes</p>
+      <button onClick={()=> setCliques(cliques + 1)}>Clique aqui</button>
     </div>
   )
 }
