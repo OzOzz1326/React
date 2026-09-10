87b338b5b44d3ba8134df6b8d2801b824bf2209c
OzOzz <caiohmbf07@gmail.com>
2026-09-10 14:02:18 -0300

    API

--- consumoAPI/src/App.jsx
+++ consumoAPI/src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App(){
 
     const[users, setUsers] = useState([])
+    const[search, setSearch] = useState("")
 
     async function searchAll(){
         const response = await fetch("https://dummyjson.com/users")
+        const data = await response.json()
+        console.log(data)
+        setUsers(data.users)
+    }
 
+    async function searchName(name){
+        const response = await fetch("https://dummyjson.com/users/search?q="+name)
         const data = await response.json()
         console.log(data)
         setUsers(data.users)
     }
 
     function showInfo(user){
-        alert("Telefone: "+user.phone+".\nEmail: "+user.email+"\nMora em: "+user.address.city)
+        alert("Telefone: "+user.phone+".\nEmail: "+user.email+"\nCidade: "+user.address.city)
     }
+
+    useEffect(()=> {
+        searchAll()
+    }, [])
 
     return (
         <div>            
             <h1>Consumo de API</h1>
             <p>Buscando dados da API DummyJSON</p>
 
+            <hr/>
+
+            <input onChange={e=> setSearch(e.target.value)} placeholder="Digite um nome..." />
+            <button onClick={()=> searchName(search)}>Pesquisar</button>
+
             <ul>
                 {
                     users.length == 0 ?
-                        <button onClick={searchAll}>Carregar dados</button>
+                        <p>Lista vazia</p>
                     :
                         users.map(
-                            i => <li> Sr(a). {i.firstName}, tem {i.age} anos.<button onClick={()=> showInfo(i)}>Ver informações</button></li>
-                        )
+                            i => <li> <img width={30} src={"https://api.dicebear.com/10.x/initials/svg?seed="+i.firstName}/> {i.gender == "male" ? "O Sr." : "A Sra."} {i.firstName}, tem {i.age} anos.<button onClick={()=> showInfo(i)}>Ver informações</button></li>)
                 }
                 
             </ul>     
---
+++ consumoAPI/src/Ordo.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function Ordo() {
+    const [pokemon, setPokemon] = useState(null);
+    const [search, setSearch] = useState("");
+
+    async function searchPokemon(name) {
+        const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`
+        );
+
+        const data = await response.json();
+
+        console.log(data);
+
+        setPokemon(data);
+    }
+
+    return (
+        <div>
+            <h1>Pokedex</h1>
+            <p>Consulte um Pokemon</p>
+
+            <hr />
+
+            <input
+                onChange={e => setSearch(e.target.value)}
+                placeholder="Digite uma criatura..."
+            />
+
+            <button onClick={() => searchPokemon(search)}>
+                Pesquisar
+            </button>
+
+            <hr />
+
+            {pokemon === null ? (
+                <p>Escreva um nome</p>
+            ) : (
+                <div>
+                    <img src={pokemon.sprites.front_default}/>
+                    <h1>Nome: {pokemon.name}</h1>
+                    <p>Tipo: {pokemon.types[0].type.name}</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Ordo;
