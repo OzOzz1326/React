5ec2666374c8d33f9597a22232b8fdd554276b1b
OzOzz <caiohmbf07@gmail.com>
2026-09-15 17:11:18 -0300

    supabase api

--- ConexaoSupabase/src/App.jsx
+++ ConexaoSupabase/src/App.jsx
@@ -1,16 +1,51 @@
+import { useEffect, useState } from "react";
 import { supabase } from "./supabase";
 
 function App(){
 
-  async function teste(){
-    const { data, error } = await supabase.from('produtos').select()
-    console.log(data)
+  const [produtos, setProdutos] = useState([])
+  const [nome, setNome] = useState("")
+  const [preco, setPreco] = useState("")
+  const [tamanho, setTamanho] = useState("")
+  const [descricao, setDescricao] = useState("")
+
+  async function insert(){
+    const obj = {
+      nome: nome,
+      preco: preco,
+      tamanho: tamanho,
+      descricao: descricao
+    }
+    const { data, error } = await supabase.from('produtos').insert(obj)
+    alert("Produto salvo")
+    document.location.reload()
   }
+
+  async function searchAll(){
+    const { data, error } = await supabase.from('produtos').select().order('id', {ascending: false})
+    console.log(data)
+    setProdutos(data)
+  }
+
+  useEffect(()=>{
+    searchAll()
+  }, [])
 
   return(
     <div>
       <h1>SupaBase</h1>
-      <button onClick={teste}>Testar</button>
+
+      <input onChange={e=> setNome(e.target.value)} placeholder="Nome do Produto" />
+      <br/>
+      <input onChange={e=> setPreco(e.target.value)} placeholder="Preço" />
+      <br/>
+      <input onChange={e=> setTamanho(e.target.value)} placeholder="Tamanho" />
+      <br/>
+      <input onChange={e=> setDescricao(e.target.value)} placeholder="Descrição (opcional)" />
+      <button onClick={insert}>Salvar</button>
+
+      {produtos.map(i=> <p>{i.nome} - R${i.preco} | Descrição: {i.descricao}</p>)}
+
     </div>
   );
 }
