fed0b99f72547312a840b8a3abc8d6ce2b5a5347
OzOzz <caiohmbf07@gmail.com>
2026-09-17 17:08:49 -0300

    supabase api

--- ConexaoSupabase/src/Empresas.jsx
+++ ConexaoSupabase/src/Empresas.jsx
@@ -1,79 +1,151 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./supabase";
+import "./Empresas.css"
 
-function Empresas(){
+function Empresas() {
 
-    const[empresas, setEmpresas] = useState([])
-    const[funcionarios, setFuncionarios] = useState([])
+    const [empresas, setEmpresas] = useState([])
+    const [showEmpresas, setShowEmpresas] = useState(true)
+    /*----------------------------------------------*/
+    const [funcionarios, setFuncionarios] = useState([])
+    const [showFuncionarios, setShowFuncionarios] = useState(false)
+    /*----------------------------------------------*/
+    const [showModal, setShowModal] = useState(false)
 
-    async function searchAllEmpresas(){
-        const {error, data} = await supabase.from("empresas").select()
+    async function searchAllEmpresas() {
+        const { error, data } = await supabase.from("empresas").select()
+
         console.log(data)
         setEmpresas(data)
     }
 
-    async function searchAllFuncionarios(){
-        const {error, data} = await supabase.from("funcionarios").select()
+    async function searchAllFuncionarios() {
+        const { error, data } = await supabase.from("funcionarios").select("*, empresas(*)")
+
         console.log(data)
         setFuncionarios(data)
     }
 
-    useEffect(()=> {
+    async function searchFuncionariosEmpresa(id_empresa){
+        const { error, data } = await supabase.from("funcionarios").select("*, empresas(*)").eq("id_empresa", id_empresa)
+
+        console.log(data)
+        setFuncionarios(data)
+
+        setView()
+    }
+
+    function setView(){
+        if(showEmpresas == true){
+            setShowEmpresas(false)
+            setShowFuncionarios(true)
+        }else{
+            setShowEmpresas(true)
+            setShowFuncionarios(false)
+        }
+    }
+
+    useEffect(() => {
         searchAllEmpresas()
         searchAllFuncionarios()
     }, [])
-    
+
     return (
         <div>
+
+            {
+                showModal == true ?
+                <div>
+                    <div onClick={()=> setShowModal(false)} className="fundoPreto"></div>
+                    
+                    <div className="modal">
+                        <h2>Novo funcionário</h2>
+                        <input placeholder="Nome"/>
+                        <br/>
+                        <input placeholder="Contato"/>
+                        <br/>
+                        <select>
+                            <option value="1">Funcionário comum</option>
+                            <option value="0">Administrador</option>
+                        </select>
+                        <br/><br/>
+                        <button>Salvar</button>
+                    </div>
+                </div>
+                :
+                <></>
+            }
+
             <h1>Relacionamento de Tabelas</h1>
             <p>Consulte na tabela empresas e funcionários </p>
 
-            <h2>Empresas</h2>
+            {
+                showEmpresas == true ?
+                    <div>
+                        <h2>Empresas</h2>
+                        <table border="true">
+                            <tr>
+                                <td>ID</td>
+                                <td>Nome</td>
+                                <td>CNPJ</td>
+                                <td>Endereço</td>
+                                <td>Ações</td>
+                            </tr>
+                            {
+                                empresas.map(i =>
+                                    <tr>
+                                        <td>{i.id}</td>
+                                        <td>{i.nome}</td>
+                                        <td>{i.cnpj}</td>
+                                        <td>{i.endereco}</td>
+                                        <td><button onClick={()=> searchFuncionariosEmpresa(i.id)}>Ver funcionários</button></td>
+                                    </tr>
+                                )
+                            }
+                        </table>
+                    </div>
+                    :
+                    <></>
+            }
 
-            <table border="true">
-                <tr>
-                    <td>ID</td>
-                    <td>Nome</td>
-                    <td>CNPJ</td>
-                    <td>Endereço</td>
-                </tr>
-                {
-                    empresas.map(i =>
-                        <tr>
-                            <td>{i.id}</td>
-                            <td>{i.nome}</td>
-                            <td>{i.cnpj}</td>
-                            <td>{i.endereco}</td>
-                        </tr>
-                    )
-                }
-            </table>
+            {
+                showFuncionarios == true ?
+                    <div>
+                        <h2>Funcionários</h2>
+                        <button onClick={()=> setShowModal(true)}>Cadastrar</button>
+                        <br/><br/>
+                        <button onClick={setView}>Voltar</button>
 
-            <h2>Funcionários</h2>
-
-            <table border="true">
-                <tr>
-                    <td>ID</td>
-                    <td>Nome</td>
-                    <td>Nome da Empresa</td>
-                    <td>Endereço da Empresa</td>
-                    <td>Cargo</td>
-                    <td>Contato</td>
-                </tr>
-                {
-                    funcionarios.map(i=>
-                        <tr>
-                    <td>{i.id}</td>
-                    <td>{i.nome}</td>
-                    <td>{i.nome}</td>
-                    <td>{i.endereco}</td>
-                    <td>{i.cargo}</td>
-                    <td>{i.contato}</td>
-                </tr>
-                    )
-                }
-            </table>
-
+                        <table border="true">
+                            <tr>
+                                <td>ID</td>
+                                <td>Nome</td>
+                                <td>Nome da Empresa</td>
+                                <td>Endereço da Empresa</td>
+                                <td>Cargo</td>
+                                <td>Contato</td>
+                            </tr>
+                            {
+                                funcionarios.map(i =>
+                                    <tr>
+                                        <td>{i.id}</td>
+                                        <td>{i.nome}</td>
+                                        <td>{i.empresas.nome}</td>
+                                        <td>{i.empresas.endereco}</td>
+                                        <td>{i.cargo == 0 ?
+                                            "Administrador"
+                                            :
+                                            "Funcionário comum"
+                                        }</td>
+                                        <td>{i.contato}</td>
+                                    </tr>
+                                )
+                            }
+                        </table>
+                    </div>
+                    :
+                        <></>
+            }
         </div>
     );
 }
